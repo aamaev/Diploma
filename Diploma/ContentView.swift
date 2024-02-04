7b169fc380732df496e1778587ba76d63e3b942b
Artem Amaev <artemamaev2@gmail.com>
2024-02-04 18:37:42 +0300

    Initial Commit

---
+++ Diploma/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Diploma
+//
+//  Created by Артём Амаев on 4.02.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Diploma/DiplomaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiplomaApp.swift
+//  Diploma
+//
+//  Created by Артём Амаев on 4.02.24.
+//
+
+import SwiftUI
+
+@main
+struct DiplomaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
